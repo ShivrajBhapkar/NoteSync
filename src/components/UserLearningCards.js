11882ca913c04de1431ea30557111ba87e0536a0
Shivraj <shivrajofficial102@gmail.com>
2023-10-24 00:10:26 +0530

    Making UI responsive

--- src/components/UserLearningCards.js
+++ src/components/UserLearningCards.js
@@ -1,65 +1,58 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
-import axios from "../axios-config"; // Import your Axios instance
-import { useNavigate, Navigate } from "react-router-dom"; // Import useNavigate for navigation
+import axios from "../axios-config";
+import { useNavigate } from "react-router-dom";
 import Button from "./ui/Button";
+
 const UserPlaylistsCards = ({ playlistInfo }) => {
     const userId = useSelector((store) => store.authentication.userId);
     const navigate = useNavigate();
-   
-     const navigateToPlaylist = () => {
+
+    const navigateToPlaylist = () => {
         navigate(`/playlist/${playlistInfo.playlistId}`);
     };
-     const untrackPlaylist = () => {
-         // Define the URL for the DELETE request
-         const deleteUrl = `/users/${userId}/trackPlaylists`;
 
-         // Define the request body with the playlistId
-         const requestBody = { playlistId: playlistInfo.playlistId };
+    const untrackPlaylist = () => {
+        const deleteUrl = `/users/${userId}/trackPlaylists`;
+        const requestBody = { playlistId: playlistInfo.playlistId };
 
-         // Make a DELETE request to untrack the playlist
-         axios
-             .delete(deleteUrl, { data: requestBody })
-             .then((response) => {
-                 // Handle the success response (e.g., update UI or state)
-                 // You might want to update the list of tracked playlists in your Redux state.
-                 // dispatch({ type: "UNTRACK_PLAYLIST", playlistId: playlistInfo.playlistId });
-                  navigate("/");
-             })
-             .catch((error) => {
-                 // Handle any errors, such as displaying an error message to the user.
-                 console.error("Error untracking playlist: ", error);
-             });
-     };
- 
-  
+        axios
+            .delete(deleteUrl, { data: requestBody })
+            .then((response) => {
+                navigate("/");
+            })
+            .catch((error) => {
+                console.error("Error untracking playlist: ", error);
+            });
+    };
+
     return (
-        <div className="relative p-2 m-2 w-72  shadow-lg overflow-hidden">
-            <img
-                className="rounded-lg"
-                alt="thumbnail"
-                src={playlistInfo.playlistImageURL}
-            />
-            <div className="flex flex-col h-full">
-                <div className="flex-grow">
-                    <ul>
-                        <li className="font-bold overflow-hidden overflow-ellipsis whitespace-nowrap">
-                            {playlistInfo.playlistTitle}
-                        </li>
-                    </ul>
-                    <div className="mt-4 space-x-2">
-                        <button
-                            onClick={navigateToPlaylist}
-                            className="bg-blue-500 text-white font-bold rounded-md py-2 px-4"
-                        >
-                            Continue
-                        </button>
-                        <Button
-                            onClick={untrackPlaylist}
-                            label="Untrack"
-                            action="secondary"
-                        />
-                    </div>
+        <div className="p-2 m-2">
+            <div className="relative w-full aspect-w-2 aspect-h-3 shadow-lg overflow-hidden">
+                <img
+                    className="object-cover rounded-lg"
+                    alt="thumbnail"
+                    src={playlistInfo.playlistImageURL}
+                />
+            </div>
+            <div className="mt-4">
+                <ul>
+                    <li className="font-bold truncate text-center">
+                        {playlistInfo.playlistTitle}
+                    </li>
+                </ul>
+                <div className="mt-2  flex sm:flex-row md:flex-row lg:flex-row xl:flex-row flex-col">
+                    <Button
+                        onClick={navigateToPlaylist}
+                        label="Continue"
+                        action="primary"
+                    />
+                     
+                    <Button
+                        onClick={untrackPlaylist}
+                        label="Untrack"
+                        action="secondary"
+                    />
                 </div>
             </div>
         </div>
--- src/components/ui/Button.js
+++ src/components/ui/Button.js
@@ -3,9 +3,9 @@ import React from "react";
 const Button = ({ onClick, label, action }) => {
     // Define predefined styles for each action
     const buttonStyles = {
-        primary: "bg-blue-500 text-white font-bold rounded-md py-2 px-4",
-        secondary: "border border-red-400 text-red-400 font-bold rounded-md py-2 px-4",
-        normal: "bg-gray-400 text-black font-bold rounded-md py-2 px-4", // Style for "normal" action
+        primary:"w-full bg-blue-500 text-white font-bold rounded-md py-2 px-4 mb-2 ml-2",
+        secondary:"w-full  border border-red-400 text-red-400 font-bold rounded-md py-2 px-4 mb-2 ml-2",
+        normal:"w-full bg-gray-400 text-black font-bold rounded-md py-2 px-4 mb-2 ml-2", // Style for "normal" action
     };
 
     // Determine the style based on the action prop
