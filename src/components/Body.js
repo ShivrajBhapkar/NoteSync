adcdc4aaf94ccaffd3863bd8d94f6ea6ce36ce61
Shivraj <shivrajofficial102@gmail.com>
2023-10-23 23:26:00 +0530

    fix : toggle issue of sidebar

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,56 +1,86 @@
-import React from 'react'
-// import Sidebar from './Sidebar'
-import { useSelector, useDispatch } from "react-redux";
-import MaineContainer from './MaineContainer'
+import React, { useEffect, useState } from "react";
 import { FaBars } from "react-icons/fa";
-import { Outlet } from 'react-router-dom'
-import { toggleMenu } from "../utils/appSlice";
-// import { useSelector } from "react-redux";
-import { ToastContainer } from 'react-toastify'
-import Sidebar from "./newSidebar"
+import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import Sidebar from "./newSidebar";
+
 const Body = () => {
- const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
- const dispatch = useDispatch();
- const handleSidebarToggle = () => {
-     // Dispatch the action to toggle the sidebar
-     dispatch(toggleMenu());
- };
-  return (
-      <div className="flex h-screen">
-          <div className={`flex-[15%] ${isMenuOpen ? "block" : "hidden"} h-[110%]`}>
-              <Sidebar />
-          </div>
-          <div className="flex-[90%]">
-              <div className="bg-[#1c212c] text-white py-4 pl-5">
-                  <div className="flex items-center justify-end">
-                      <button
-                          onClick={handleSidebarToggle}
-                          className="text-xl focus:outline-none mr-4  md:hidden lg:hidden xl:hidden"
-                      >
-                          <FaBars />
-                      </button>
-                  </div>
-              </div>
+    const [showSidebar, setShowSidebar] = useState(true);
 
-              <div className="px-5 pt-2 h-[100%] overflow-y-hidden">
-                  <Outlet />
-              </div>
-          </div>
-          <ToastContainer
-              position="top-right"
-              autoClose={5000}
-              hideProgressBar={false}
-              newestOnTop={false}
-              closeOnClick
-              rtl={false}
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-              theme="light"
-          />
-          <ToastContainer />
-      </div>
-  );
-}
+    const handleSidebarToggle = () => {
+        
+        setShowSidebar(!showSidebar);
+    };
 
-export default Body
+    const handleHamburgerClick = () => {
+        if (window.innerWidth <= 768) {
+            console.log(window.innerWidth)
+            handleSidebarToggle(); // Toggle the sidebar when the screen is less than medium
+        }
+    };
+
+    useEffect(() => {
+        // Check the window width and update the showSidebar state
+        const handleWindowResize = () => {
+            if (window.innerWidth <= 768) {
+                // Adjust the width as per your medium breakpoint
+                setShowSidebar(false);
+            } else {
+                setShowSidebar(true);
+            }
+        };
+
+        // Add a listener for window resize
+        window.addEventListener("resize", handleWindowResize);
+
+        // Initialize showSidebar on component mount
+        handleWindowResize();
+
+        // Clean up the listener on component unmount
+        return () => {
+            window.removeEventListener("resize", handleWindowResize);
+        };
+    }, []);
+
+    return (
+        <div className="flex h-screen">
+            <div
+                className={`md:flex-[15%] h-[110%] ${
+                    showSidebar ? "block" : "hidden"
+                }`}
+            >
+                <Sidebar />
+            </div>
+            <div className="flex-[90%]">
+                <div className="bg-[#1c212c] text-white py-4 pl-5">
+                    <div className="flex items-center justify-end">
+                        <button
+                            onClick={handleHamburgerClick}
+                            className={`text-xl focus:outline-none mr-4 md:hidden lg:hidden xl:hidden`}
+                        >
+                            <FaBars />
+                        </button>
+                    </div>
+                </div>
+                <div className="px-5 pt-2 h-[100%] overflow-y-hidden">
+                    <Outlet />
+                </div>
+            </div>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
+            <ToastContainer />
+        </div>
+    );
+};
+
+export default Body;
